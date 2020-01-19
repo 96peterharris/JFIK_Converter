344ace596aa400f968fb30a6a2e6f7c30eaab028
Niewidzialny <niewidzialny84@vp.pl>
2020-01-19 21:43:29 +0100

    .idea deletion

--- src/com/pioterDeveloper/Main.java
+++ src/com/pioterDeveloper/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         OpenFile input = new OpenFile();
         input.openFIle("test.eps");
-        String workString = input.getContent();
-        System.out.println(workString);
+        //String workString = input.getContent();
+       // System.out.println(workString);
         ContentFinder finder = new ContentFinder();
-        finder.findShapes(workString);
-        finder.printShapeBase();
+        //finder.findShapes(workString);
+        //finder.printShapeBase();
         GenerateSVG svgGenerator = new GenerateSVG();
         svgGenerator.convert(finder.getShapeBase(),"test.svg");
         GenerateCanvas canvasGenerator = new GenerateCanvas();
